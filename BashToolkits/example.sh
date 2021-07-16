92fd107fd57c33821e7601b1f445a26663eb3b82
Chengwei Wang <wangchw8@outlook.com>
2021-07-16 12:59:28 +0100

    BashT: Wiznote Terminal exec + config + prompt + sh practice
    Tips: Actually we should commit once for a chapter

---
+++ BashToolkits/example.sh
@@ -0,0 +1,91 @@
+# #!/bin/bash
+# Tips: `> output`
+
+
+########## Basic Pcp and Mt ##########
+#[commands separate by lines and {}. end with ; in one sentence]
+[command line1]; [command line2]; [command line3]
+
+########## How to make the shellscript with options ##########
+
+
+########## Mathematical Calculation ##########
+
+var2=2var=$((1+1*2**$var2-12/2)) # the order is ok
+
+########## Assign variables ##########
+set variablesvar = $(command to come up an object) # method1 (default and recommended)
+echo ${var := "BPSM"} # method2
+var2 = ${command with $var} # you could use the var to create another var
+
+# use variables
+echo $var 
+# Use the self-defined var
+echo ${var}"is fecth" 
+
+# {} set the scale of the var. you can also use the $var in the commends and some situation should be considered.
+# clear variables
+unset var # No $ should be used with the unset command when pointing to var
+# get the length of string variable
+${#string}  
+## e.g.
+var="str"
+echo ${#var}
+> 3
+
+# extract substring from the string variable
+${string:position:length} 
+## e.g.
+var="stella"
+echo ${var:4}
+> stel
+echo ${var:4:2}
+> st
+
+# delete the shortest/first total match of string variable
+# Tips: append character every time to match the pattern and once match, then delete this part and return the reset of the string variable
+${string#matchPattern}  # from front of the string
+${string%matchPattern}  # from back of the string
+## e.g
+var="bpsm.get.txt"
+var1="b.bpsm.get.txt"
+echo ${var#*.}
+> get.txt
+echo ${var1#?.}
+> bpsm.get.txt
+echo ${var%.*}
+> bpsm.get
+
+# delete the longest/last total match of string variable
+# Tips: the same but the last one would be considered as matched and delete
+${string##matchPattern}  # from front of the string
+${string%%matchPattern}  # from back of the string
+# e.g
+var="bpsm.get.txt"
+var1="b.bpsm.get.txt"
+echo ${var##*.}
+> txt
+echo ${var1%%.*}
+> b
+echo ${var%%.*}
+> bpsm.get
+
+# Replace string in string variable
+${var/pattern/replacement}  # replace the first and longest match
+${var//pattern/replacement}  # replace all match
+${var/#pattern/replacement} # match from the front of the string
+${var/%pattern/replacement} # match from the end of the string
+# Tips: Remember the var is not changed by this action and you should assign this to antoher variable.
+# e.g.
+var="go.exe.exe.zip.exe"
+echo ${var/.e*./}
+> goexe
+echo ${var/.e*./.tar.}
+> go.tar.exe
+echo ${var//.e*./.tar.}
+> go.tar.exe
+var="go.exe.tarexe.zip.exe"
+echo ${var//.exe./.tar.}
+> go.tar.tarexe.zip.exe
+
+#################### Control Structure ##################### Judge## if if [condition]then	[commands]elif	[commands]else	[commands]fi # it is necessary for the bash shell# Loop## for loopfor x in [sequences]; do[commands] # temp var would be specified as $xdone## e.g.for x in {a..z}; doegrep -o 'D. '$x'\S+' dros_list.txt > $x'_species.txt'## while loop# Tips: the variables are separated by the IFSwhile read [local variables]do[contents]done < [input]## e.g.1ls -1 *.txt > myfiles.listcat myfiles.listwhile read myfilenamedoecho -e "Processing $myfilename..."linesinfile=$(wc -l $myfilename | cut -d ' ' -f1)echo -e "\thas $linesinfile lines in it"done <  myfiles.list## e.g.2 count the line in different documentswhile read myfilenamedoecho -e "Processing $myfilename..."linesinfile=$(wc -l $myfilename | cut -d ' ' -f1)echo -e "\thas $linesinfile lines in it"done <  myfiles.list#################### Fucntions ####################function x(){	# $1 and $2 will follow the input order}# 
