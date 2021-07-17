e517221dfe6c122ee91fc3f2a33899bcb2174600
Chengwei Wang <wangchw8@outlook.com>
2021-07-17 06:28:15 +0100

    BashT: Reformat example.sh

--- BashToolkits/example.sh
+++ BashToolkits/example.sh
@@ -2,18 +2,18 @@
 # Tips: `> output`
 
 
-########## Basic Pcp and Mt ##########
+#################### Basic Pcp and Mt ####################
 #[commands separate by lines and {}. end with ; in one sentence]
 [command line1]; [command line2]; [command line3]
 
-########## How to make the shellscript with options ##########
+#################### How to make the shellscript with options ####################
 
 
-########## Mathematical Calculation ##########
+#################### Mathematical Calculation ####################
 
 var2=2var=$((1+1*2**$var2-12/2)) # the order is ok
 
-########## Assign variables ##########
+#################### Assign variables ####################
 set variablesvar = $(command to come up an object) # method1 (default and recommended)
 echo ${var := "BPSM"} # method2
 var2 = ${command with $var} # you could use the var to create another var
@@ -88,4 +88,51 @@ var="go.exe.tarexe.zip.exe"
 echo ${var//.exe./.tar.}
 > go.tar.tarexe.zip.exe
 
-#################### Control Structure ##################### Judge## if if [condition]then	[commands]elif	[commands]else	[commands]fi # it is necessary for the bash shell# Loop## for loopfor x in [sequences]; do[commands] # temp var would be specified as $xdone## e.g.for x in {a..z}; doegrep -o 'D. '$x'\S+' dros_list.txt > $x'_species.txt'## while loop# Tips: the variables are separated by the IFSwhile read [local variables]do[contents]done < [input]## e.g.1ls -1 *.txt > myfiles.listcat myfiles.listwhile read myfilenamedoecho -e "Processing $myfilename..."linesinfile=$(wc -l $myfilename | cut -d ' ' -f1)echo -e "\thas $linesinfile lines in it"done <  myfiles.list## e.g.2 count the line in different documentswhile read myfilenamedoecho -e "Processing $myfilename..."linesinfile=$(wc -l $myfilename | cut -d ' ' -f1)echo -e "\thas $linesinfile lines in it"done <  myfiles.list#################### Fucntions ####################function x(){	# $1 and $2 will follow the input order}# 
+#################### Control Structure ####################
+########## Judge ##########
+## if 
+if [condition]
+then
+	[commands]
+elif
+	[commands]
+else	
+    [commands]
+fi # it is necessary for the bash shell
+
+########## Loop ##########
+# for loop
+for x in [sequences]; do[commands]  # temp var would be specified as $x done
+## e.g.
+for x in {a..z}; 
+do 
+egrep -o 'D. '$x'\S+' dros_list.txt > $x'_species.txt' 
+
+# while loop
+## Tips: the variables are separated by the IFS
+while read [local variables] 
+do
+[contents]
+done < [input]
+## e.g.
+1ls -1 *.txt > myfiles.list
+cat myfiles.list
+while read myfilename
+do
+echo -e "Processing $myfilename..."
+linesinfile=$(wc -l $myfilename | cut -d ' ' -f1)
+echo -e "\thas $linesinfile lines in it"
+done <  myfiles.list#
+# e.g.2 count the line in different documents
+while read myfilename
+do
+echo -e "Processing $myfilename..."
+linesinfile=$(wc -l $myfilename | cut -d ' ' -f1)
+echo -e "\thas $linesinfile lines in it"
+done <  myfiles.list
+
+#################### Fucntions ####################
+function x(){	
+	# $1 and $2 will follow the input order
+	$1 + $2
+	}# 
