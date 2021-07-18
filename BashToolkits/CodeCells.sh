020f1209153da6c88beee85fe640f5516b28dd30
Chengwei Wang <wangchw8@outlook.com>
2021-07-18 10:17:15 +0100

    BashT: Permission

--- BashToolkits/CodeCells.sh
+++ BashToolkits/CodeCells.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-#$ wc [files] # wildcards would also be acceptable arguments
-# Options
-#-l # count the lines in the files, only lines number, not similar to `ls`-w # count the words in the files
+# su
+## F: assume the identity of another user and either start a new shell session with that user's ID, or to issue a single command as that user. 
+#$ su [options] [-] [user_name] 
+# and then type the passwd would login in the administrator account
+## Options
+#-l, --login # login shell and change the home dir to the user in the argument. Default Superuser.
+#-c, --command '<command>' # exec the command with the permissions of specified user. Tips: enclose the command in quotes
+
+# sudo
+## F: set up a configuration file called /etc/sudoers and define specific commands that particular users are permitted to execute as a different user (usually the superuser) under an assumed identity.
+## Ab: 
+## 1. sudo does not require access to the superuser's password, but requires the user’s own password.
+## 2. does not start a new shell, nor does it load another user's environment
+#$ sudo [commands]  # use the administrator authority for particular commands
+## Options
+#-i  # start an interactive superuser session
+#-l  # see the priviledge of sudo
