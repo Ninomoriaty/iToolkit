1282e09a147fa82377f0baa296731bfc0d23d4b0
Chengwei Wang <wangchw8@outlook.com>
2021-07-18 15:52:56 +0100

    BashT: I/O Redirection

--- BashToolkits/CodeCells.sh
+++ BashToolkits/CodeCells.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# su
-## F: assume the identity of another user and either start a new shell session with that user's ID, or to issue a single command as that user. 
-#$ su [options] [-] [user_name] 
-# and then type the passwd would login in the administrator account
-## Options
-#-l, --login # login shell and change the home dir to the user in the argument. Default Superuser.
-#-c, --command '<command>' # exec the command with the permissions of specified user. Tips: enclose the command in quotes
 
-# sudo
-## F: set up a configuration file called /etc/sudoers and define specific commands that particular users are permitted to execute as a different user (usually the superuser) under an assumed identity.
-## Ab: 
-## 1. sudo does not require access to the superuser's password, but requires the user’s own password.
-## 2. does not start a new shell, nor does it load another user's environment
-#$ sudo [commands]  # use the administrator authority for particular commands
+#$ echo [string]
+
+echo test is good  # even without the quotes
+# echo usage
+echo 'Mesages for users'now=$("date")
+echo -e 'Date: \n $now'
+echo -e "Date: \n $now"
+echo -e "Date: \n \$now"
+
 ## Options
-#-i  # start an interactive superuser session
-#-l  # see the priviledge of sudo
+#-e  # enable interpretation of following backslash escapes
