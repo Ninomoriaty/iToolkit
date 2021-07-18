14c5f8f29ece6efe90d78540896fc08e030b5762
Chengwei Wang <wangchw8@outlook.com>
2021-07-18 08:14:28 +0100

    BashT: Linux file system part

--- BashToolkits/CodeCells.sh
+++ BashToolkits/CodeCells.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "This apple\'s price is $2300"
-## Outputs: This apple's price is
-echo This apple\'s price is \$2300 \& it is wonderful\! \\\\
-## Outputs: This apple's price is $2300 & it is wonderful! \\
+#$ wc [files] # wildcards would also be acceptable arguments
+# Options
+#-l # count the lines in the files, only lines number, not similar to `ls`-w # count the words in the files
